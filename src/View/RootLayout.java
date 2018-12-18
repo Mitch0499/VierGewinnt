c8c36ca69dcd65fb485405e64680235f4988350b
Tamara <Tamara@10.30.236.251>
2018-12-18 19:26:41 +0100

    Gewinnerermittlung und weiteres

--- src/View/RootLayout.java
+++ src/View/RootLayout.java
@@ -1,5 +1,19 @@
 package View;
 
+import VierGewinnt.MainApp;
+
 public class RootLayout {
+	
+	// Reference to the main application
+    private MainApp mainApp;
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
 
 }
